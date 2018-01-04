c02de1124aa5bb9222dbbbb9bf0a25d8a116f06e
tf <firemumu@qq.com>
2018-01-04 09:37:29 +0800

    新增主页

---
+++ src/main/java/com/excgo/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.excgo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
